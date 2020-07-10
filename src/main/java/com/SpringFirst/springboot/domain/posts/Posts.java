8ca2d57ea089d8580248df89641f4bc39077a4d8
joochangyong <51350374+joochangyong@users.noreply.github.com>
2020-07-10 18:03:04 +0900

    20200710_3장_API등록

---
+++ src/main/java/com/SpringFirst/springboot/domain/posts/Posts.java
@@ -0,0 +1,38 @@
+package com.SpringFirst.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter //클래스 내 모든 필드의 Getter메소드를 자동 생성
+@NoArgsConstructor //기본 생성자 자동 추가. public Posts() {}와 같은 효과
+@Entity //테이블링과 링크될 클래스임을 나타냄.
+public class Posts {
+    @Id //해당 테이블의  pk필드.
+    //pk의 생성 규칙.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    //칼럼 생성.
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder //해당 클래스의 필더 패턴 클래스를 생성.
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/SpringFirst/springboot/web/HelloController.java
@@ -0,0 +1,22 @@
+package com.SpringFirst.springboot.web;
+
+import com.SpringFirst.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    //@RequestParam : 외부에서 API로 넘긴 파라미터를 갖져오는 어노테이션.
+    //여기에서는 외부에서 name(@RequestParam("name"))이란 이름으로 넘긴 파라미터를 메소드 파라미터 name(String name)에 저장하게 됨.
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
