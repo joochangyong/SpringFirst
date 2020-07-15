63acccea82ca90cb40f429f08af47143e4175e7a
joochangyong <51350374+joochangyong@users.noreply.github.com>
2020-07-15 14:53:48 +0900

    20200713_4장_게시글완료

--- src/main/java/com/SpringFirst/springboot/service/posts/PostsService.java
+++ src/main/java/com/SpringFirst/springboot/service/posts/PostsService.java
@@ -2,12 +2,15 @@ package com.SpringFirst.springboot.service.posts;
 
 import com.SpringFirst.springboot.domain.posts.Posts;
 import com.SpringFirst.springboot.domain.posts.PostsRepository;
+import com.SpringFirst.springboot.web.dto.PostsListResponseDto;
 import com.SpringFirst.springboot.web.dto.PostsResponseDto;
 import com.SpringFirst.springboot.web.dto.PostsSaveRequestDto;
 import com.SpringFirst.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -24,7 +27,7 @@ public class PostsService {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
-        
+
         return id;
     }
 
@@ -32,5 +35,19 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    //readOnly = true : 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선됙 때문에 등록, 수정, 삭제 기능이 전혀 없는 서비스 메소드에서 사용하는 것을 추천.
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) //.map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
