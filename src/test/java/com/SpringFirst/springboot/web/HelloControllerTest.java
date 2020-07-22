5b582920437dd86c3872c3017284745cd8014904
joochangyong <51350374+joochangyong@users.noreply.github.com>
2020-07-22 15:46:54 +0900

    5장완료

--- src/test/java/com/SpringFirst/springboot/web/HelloControllerTest.java
+++ src/test/java/com/SpringFirst/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.SpringFirst.springboot.web;
 
+import com.SpringFirst.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,7 +22,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 //여러 스프링 테스트 어노테이션 중, Web(Spring MVC)에 집중할 수 있는 어노테이션.
 //선언할 경우 @controller, @ControllerAdvice등을 사용 가능. 여기서는 컨트롤러만 사용하기 때문에 선언.
-@WebMvcTest(controllers = HelloController.class)
+//@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+            @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     //스프링이 관리하는 빈(Bean)을 주입 받는다.
@@ -26,6 +35,7 @@ public class HelloControllerTest {
     //웹 API를 테스트할 때 사용. 스프링 MVC 테스트의 시작점. 이 클래스를 통해 HTTP GET,POST 등에 대한 API테스트가 가능.
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -38,6 +48,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
