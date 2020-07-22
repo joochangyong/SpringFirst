3f029678d496553c6c2fa093e379394e26226f43
joochangyong <51350374+joochangyong@users.noreply.github.com>
2020-07-22 14:57:42 +0900

    네이버로그인

--- src/main/java/com/SpringFirst/springboot/web/Indexcontroller.java
+++ src/main/java/com/SpringFirst/springboot/web/Indexcontroller.java
@@ -1,5 +1,6 @@
 package com.SpringFirst.springboot.web;
 
+import com.SpringFirst.springboot.config.auth.LoginUser;
 import com.SpringFirst.springboot.config.auth.dto.SessionUser;
 import com.SpringFirst.springboot.service.posts.PostsService;
 import com.SpringFirst.springboot.web.dto.PostsResponseDto;
@@ -19,11 +20,13 @@ public class Indexcontroller {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) { //Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음. 여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달.
+    //@LoginUser : 기존에 (User) httpSession.getAttribute("user")로 가져오던 세션 정보값이 개선.
+    //어느 컨트롤러에서든 @LoginUser만 사용하면 세션 정보를 가져올 수 있음.
+    public String index(Model model, @LoginUser SessionUser user) { //Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음. 여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달.
         model.addAttribute("posts", postsService.findAllDesc());
 
         //로그인 성공시 세션에 SessionUser를 저장.
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) { //세션에 저장된 값이 있을때만 model에 userName으로 등록.
             model.addAttribute("userName", user.getName());
         }
